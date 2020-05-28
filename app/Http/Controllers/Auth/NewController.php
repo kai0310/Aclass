172b07b51998eaf166489ee0b8d1e7b18d2c954f
asuchi0819 <asuchi0819@gmail.com>
2020-05-28 21:56:31 +0900

    Validatorの不具合の修正

--- app/Http/Controllers/Auth/NewController.php
+++ app/Http/Controllers/Auth/NewController.php
@@ -4,26 +4,13 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class NewController extends Controller
 {
   public function new(Request $request){
-
-    $messages = [
-    'same'    => 'The :attribute and :other must match.',
-    'size'    => 'The :attribute must be exactly :size.',
-    'between' => 'The :attribute value :input is not between :min - :max.',
-    'in'      => 'The :attribute must be one of the following types: :values',
-];
-
-    Validator::make($request, [
-      'hash_login_id' => ['required', 'numeric'],
-      'temporary_password' => ['required', 'numeric'],
-      'password' => ['required', 'string', 'min:8', 'confirmed']
-    ], $messages);
 
     $request->validate([
       'hash_login_id' => ['required', 'numeric'],
