a053038188b17d85bd5570929a11d21376b47dcf
Teraterayuki <tenko0316yuki1@gmail.com>
2020-05-29 15:18:23 +0900

    提出期限の迫っている課題の表示を追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,44 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Submission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Schedule;
 use App\User;
+use App\Task;
 
 class HomeController extends Controller
 {
-  /**
-  * Create a new controller instance.
-  *
-  * @return void
-  */
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-  /**
-  * Show the application dashboard.
-  *
-  * @return \Illuminate\Contracts\Support\Renderable
-  */
-  public function index()
-  {
-    $thirtyDaysSchedules = Schedule::whereDate('time', '>=', date("Y-m-d"))->whereDate('time', '<=', date("Y-m-d",strtotime("+30 day")))->orderBy('time', 'asc')->pluck('id');
-    $allGroupSchedules = [];
-    foreach(Auth::user()->group as $group){
-      foreach($group->schedules as $schedule){
-        $allGroupSchedules[] = $schedule['id'];
-      }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $thirtyDaysSchedules = Schedule::whereDate('time', '>=', date("Y-m-d"))->whereDate('time', '<=', date("Y-m-d",strtotime("+30 day")))->orderBy('time', 'asc')->pluck('id');
+        $allGroupSchedules = [];
+        $allGroupId = [];
+        foreach(Auth::user()->group as $group){
+            foreach($group->schedules as $schedule){
+                $allGroupSchedules[] = $schedule['id'];
+            }
+            $allGroupId[]= $group->id;
+        }
+        $thirtyDaysGroupSchedules = [];
+        foreach($thirtyDaysSchedules as $schedule){
+            if(in_array($schedule, $allGroupSchedules)){
+                $thirtyDaysGroupSchedules[] = $schedule;
+            }
+        }
+        $schedules = Schedule::whereIn('id', $thirtyDaysGroupSchedules)->with(['groups'])->get();
+        $thirtyDaysTasks = Task::whereDate('limit', '>=', date("Y-m-d"))->whereDate('limit', '<=', date("Y-m-d",strtotime("+30 day")))->whereIn('group_id',$allGroupId)->orderBy('limit', 'asc')->get();
+        $taskId = [];
+        foreach ($thirtyDaysTasks as $thirtyDaysTask){
+            $taskId[]=$thirtyDaysTask["id"];
+        }
+
+        return view('pages.home', ['schedules' => $schedules,'tasks'=>$thirtyDaysTasks]);
     }
-    $thirtyDaysGroupSchedules = [];
-    foreach($thirtyDaysSchedules as $schedule){
-      if(in_array($schedule, $allGroupSchedules)){
-        $thirtyDaysGroupSchedules[] = $schedule;
-      }
-    }
-    $schedules = Schedule::whereIn('id', $thirtyDaysGroupSchedules)->with(['groups'])->get();
-    return view('pages.home', ['schedules' => $schedules]);
-  }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,39 +1,51 @@
 @extends('layouts.common')
 
 @section('style')
-<style>
-.schedule{margin:1rem 0}
-h3{font-size:1.5rem}
-h4{font-size:1.1rem;line-height:1.5rem}
-.group{background:#005;color:#FFF;padding:.25rem .5rem;border-radius:.25rem;font-size:.9rem}
-.schedule{position:relative}
-<?php foreach($schedules as $schedule) { ?>
-  .schedule#schedule{{$schedule['id']}}::before{display:block;background:#FFF;position:absolute;top:1.5rem;opacity:0;transition:all .25s;content:'{{decryptData($schedule['description'], 'DATA_KEY')}}'}
-  .schedule#schedule{{$schedule['id']}}:hover::before{opacity:1}
-  <?php } ?>
-</style>
+    <style>
+        .cardItem{margin:1rem 0}
+        h3{font-size:1.5rem}
+        h4{font-size:1.1rem;line-height:1.5rem}
+        .group{background:#005;color:#FFF;padding:.25rem .5rem;border-radius:.25rem;font-size:.9rem}
+        .cardItem{position:relative}
+        <?php foreach($schedules as $schedule) { ?>
+  .cardItem#schedule{{$schedule['id']}}::before{display:block;background:#FFF;position:absolute;top:1.5rem;opacity:0;transition:all .25s;content:'{{decryptData($schedule['description'], 'DATA_KEY')}}'}
+        .cardItem#schedule{{$schedule['id']}}:hover::before{opacity:1}
+        <?php } ?>
+    </style>
 @endsection
 
 @section('content')
-<div class=card>
-  {{decryptData(Auth::user()->name, 'USER_KEY')}}さん。お帰りなさい。<br />
-  ログインID：{{decryptData(Auth::user()->login_id, 'USER_KEY')}}<br />
-  識別ID：{{Auth::id()}}
-</div>
-<div class=card>
-  <h3>直近の予定</h3>
-  <?php $i = 0; ?>
-  <?php foreach($schedules as $schedule){ ?>
-    <div class="schedule omit" id="schedule{{$schedule['id']}}">
-      <h4 class=omit>{{date('n / j', strtotime($schedule['time']))}} - {{decryptData($schedule['name'], 'DATA_KEY')}}</h4>
-      <?php foreach($schedule->groups as $group){ ?>
-        <span class=group>{{decryptData($group['name'], 'DATA_KEY')}}</span>
-        <?php $i++; ?>
-      <?php } ?>
+    <div class=card>
+        {{decryptData(Auth::user()->name, 'USER_KEY')}}さん。お帰りなさい。<br />
+        ログインID：{{decryptData(Auth::user()->login_id, 'USER_KEY')}}<br />
+        識別ID：{{Auth::id()}}
     </div>
-  <?php } ?>
-  <?php if($i===0){ ?>
-    <div class=schedule><h4>直近30日間に予定はありません</h4></div>
-  <?php } ?>
-</div>
+    <div class=card>
+        <h3>直近の予定</h3>
+        <?php foreach($schedules as $schedule){ ?>
+        <div class="cardItem omit" id="schedule{{$schedule['id']}}">
+            <h4 class=omit>{{date('n / j', strtotime($schedule['time']))}} - {{decryptData($schedule['name'], 'DATA_KEY')}}</h4>
+            <?php foreach($schedule->groups as $group){ ?>
+            <span class=group>{{decryptData($group['name'], 'DATA_KEY')}}</span>
+            <?php } ?>
+        </div>
+        <?php } ?>
+        <?php if(count($schedules)===0){ ?>
+        <div class=cardItem><h4>直近30日間に予定はありません</h4></div>
+        <?php } ?>
+    </div>
+    <div class="card">
+        <h3>提出期限が迫っている課題</h3>
+        <?php foreach ($tasks as $task){ ?>
+        <div class="cardItem omit">
+            <a href="{{route('taskSingle',['task_id'=>$task['id']])}}">
+                <h4 class=omit>{{date('n月j日H時i分まで', strtotime($task['limit']))}} - {{decryptData($task['title'], 'DATA_KEY')}}</h4>
+            </a>
+        </div>
+        <?php }
+        if(count($tasks)===0){
+        ?>
+        <div class=cardItem><h4>提出期限の迫った課題はありません</h4></div>
+        <?php } ?>
+    </div>
 @endsection
